8b5bc2a7fee84f5e4d3150a09425ae88a022b0a9
Ben Parsons <ben@bpulse.co.uk>
2012-07-05 15:02:59 +0200

    working version with Console runner

--- LocaleSifter/LocaleSifter.cs
+++ LocaleSifter/LocaleSifter.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LocaleSifter
 {
-    class LocaleSifter
+    public class LocaleSifter
     {
+        public Dictionary<string, Dictionary<string, string>> LocaleContent { get; set; }
+
+        public LocaleSifter()
+        {
+            LocaleContent = new Dictionary<string, Dictionary<string, string>>();
+        }
+
+        public bool LoadResxFile(string path)
+        {
+            XDocument xDocument = XDocument.Load(path);
+            List<XElement> elements = xDocument.Root.Elements("data").ToList();
+            Dictionary<string, string> localElements = new Dictionary<string, string>();
+            foreach (XElement element in elements)
+            {
+                localElements.Add(element.Attribute("name").Value, element.Element("value").Value);
+            }
+            LocaleContent.Add(path, localElements);
+            return true;
+        }
+
+        public Dictionary<string, List<string>> MissingElements()
+        {
+            Dictionary<string, List<string>> missingElements = new Dictionary<string, List<string>>();
+            
+            for (int i = 1; i < LocaleContent.Count; i++)
+            {
+                Dictionary<string, string> translatedElements = LocaleContent.ElementAt(i).Value;
+                List<string> missingElementsForLocale = new List<string>();
+                foreach (string original in LocaleContent.ElementAt(0).Value.Keys)
+                {
+                    if (!translatedElements.ContainsKey(original))
+                        missingElementsForLocale.Add(original);
+                }
+                missingElements.Add(LocaleContent.ElementAt(i).Key, missingElementsForLocale);
+            }
+            return missingElements;
+        }
     }
 }
--- LocaleSifterConsole/Program.cs
+++ LocaleSifterConsole/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocaleSifter;
 
 namespace LocaleSifterConsole
 {
@@ -10,6 +12,40 @@ namespace LocaleSifterConsole
     {
         static void Main(string[] args)
         {
+            LocaleSifter.LocaleSifter localSifter = new LocaleSifter.LocaleSifter();
+            string english = @"C:\dev\smartFE\761\Core\Resources\QuodResource.resx";
+            string italian = @"C:\dev\smartFE\761\Core\Resources\QuodResource.it.resx";
+
+            localSifter.LoadResxFile(english);
+            localSifter.LoadResxFile(italian);
+            Dictionary<string, string> englishResults = localSifter.LocaleContent[english];
+            foreach (var item in englishResults)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+            Console.ReadKey();
+            Console.WriteLine("==============================================");
+            Dictionary<string, List<string>> missingElements = localSifter.MissingElements();
+            foreach (var locale in missingElements)
+            {
+                Console.WriteLine(locale.Key);
+                foreach (string missingItem in locale.Value)
+                {
+                    Console.Write(missingItem + ", ");
+                }
+            }
+            Console.ReadKey();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("\"Quod Key\",\"EnglishTranslation\"");//,\"Italian\"");
+            List<string> items = missingElements.ElementAt(0).Value;
+            
+            foreach (string missingItem in items)
+            {
+                sb.AppendLine("\"" + missingItem + "\",\"" + localSifter.LocaleContent[english][missingItem] + "\"");
+            }
+            TextWriter tw = new StreamWriter("output.csv");
+            tw.WriteLine(sb);
+            tw.Close();
         }
     }
 }
